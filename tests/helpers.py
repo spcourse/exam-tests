6c6f4fe7faa977505af8a76d101f6618bdd4f5ff
Simon Pauw <simonpauw@gmail.com>
2022-10-10 12:52:27 +0200

    sp1 exam tests block 1, 22/23

--- tests/helpers.py
+++ tests/helpers.py
@@ -11,6 +11,31 @@ ORDINALS = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th
             '20th', '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th',
             '29th', '30th', '31st']
 
+
+def test_direct_compare_fn(test, file_name, function_name, input, output, atol = None):
+    def testMethod():
+        try:
+            _fn = lib.getFunction(function_name, file_name)
+            _output = apply_function(_fn, input, [type(output)])
+        except InvalidFunctionApplication as e:
+            return False, e.message
+        except Exception as e:
+            return False, f"An error occured while running the function: \n {type(e).__name__}: {str(e)}"
+        if atol == None:
+            if _output != output:
+                return False, f"Expected output: {output}, not {_output}"
+        else:
+            if not similar(_output,output,atol):
+                return False, f"Expected output: {output}, not {_output}"
+
+
+        return True, ""
+
+
+    test.test = testMethod
+    test.description = lambda : f"Test {function_name} with {input}"
+    test.timeout = lambda : 90
+
 def apply_function(fn, input, output_descr, check_none=True):
     number_of_return_values = len(output_descr)
     _output = fn(*input)
@@ -34,31 +59,31 @@ def apply_function(fn, input, output_descr, check_none=True):
 
 
 def fn_string(fname, args):
-	return f"{fname}({', '.join(args)})"
+    return f"{fname}({', '.join(args)})"
 
 def has_function(test, file_name, function_name, expected_args):
-	def testMethod():
-		if assertlib.fileContainsFunctionDefinitions(file_name, function_name):
-			fn = lib.getFunction(function_name, file_name)
-			provided_args = fn.arguments
-			if len(provided_args) == len(expected_args):
-				return True, ""
-			else:
-				if len(expected_args) == 1:
-					m1 = f"Expected a single argument: {fn_string(function_name, expected_args)}"
-				else:
-					m1 = f"Expected {len(expected_args)} arguments: {fn_string(function_name, expected_args)}"
-				if len(provided_args) == 1:
-					m2 = f"Your function takes a single argument: {fn_string(function_name, provided_args)}"
-				else:
-					m2 = f"Your function takes {len(provided_args)} arguments: {fn_string(function_name, provided_args)}"
+    def testMethod():
+        if assertlib.fileContainsFunctionDefinitions(file_name, function_name):
+            fn = lib.getFunction(function_name, file_name)
+            provided_args = fn.arguments
+            if len(provided_args) == len(expected_args):
+                return True, ""
+            else:
+                if len(expected_args) == 1:
+                    m1 = f"Expected a single argument: {fn_string(function_name, expected_args)}"
+                else:
+                    m1 = f"Expected {len(expected_args)} arguments: {fn_string(function_name, expected_args)}"
+                if len(provided_args) == 1:
+                    m2 = f"Your function takes a single argument: {fn_string(function_name, provided_args)}"
+                else:
+                    m2 = f"Your function takes {len(provided_args)} arguments: {fn_string(function_name, provided_args)}"
 
-				return False, f"Incorrect number of arguments:\n\t{m1}\n\t{m2}"
-		else:
-			return False, "Function not defined"
+                return False, f"Incorrect number of arguments:\n\t{m1}\n\t{m2}"
+        else:
+            return False, "Function not defined"
 
-	test.test = testMethod
-	test.description = lambda : f"Defines the function `{function_name}()`"
+    test.test = testMethod
+    test.description = lambda : f"Defines the function `{function_name}()`"
 
 
 
