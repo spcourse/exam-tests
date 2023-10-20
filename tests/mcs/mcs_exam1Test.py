84c9a6a54b3221d5599f4657339cd5c06e45d362
Simon Pauw <simonpauw@gmail.com>
2023-10-20 15:53:40 +0200

    mcs and pdp tests

--- tests/mcs/mcs_exam1Test.py
+++ tests/mcs/mcs_exam1Test.py
@@ -1,11 +1,9 @@
 from checkpy import *
-import typing
+
+download("film.csv", "https://raw.githubusercontent.com/uvaai/pdp/2023/exam/data/film.csv")
 
 ######## Q1 ########
 
-# Only PDP
-
-######## Q2 ########
 
 fun2_def = (declarative
     .function("is_palindrome")
@@ -18,7 +16,7 @@ test2_2 = test()(fun2_def.call("hello").returns(False))
 test2_3 = test()(fun2_def.call("level").returns(True))
 
 
-######## Q3 ########
+######## Q2 ########
 
 fun3_def = (declarative
     .function("calculate_growth")
@@ -45,3 +43,16 @@ population_2 = {
 expected = {'Houston': -100000, 'Chicago': 100000, 'Los Angeles': 100000, 'New York': 200000}
 
 test3_1 = test()(fun3_def.call(population_1, population_2).returns(expected))
+
+
+######## Q3 ########
+
+fun4_def = (declarative
+    .function("average_popularity_year")
+    .params("filename", "year")
+)
+fun4_def_float = fun4_def.returnType(float)
+
+test4_1 = test()(fun4_def_float.call("film.csv", 1984).returns(approx(42.47, abs=0.1)))
+test4_2 = test()(fun4_def_float.call("film.csv", 1990).returns(approx(39.42, abs=0.1)))
+test4_3 = test()(fun4_def.call("film.csv", 1994).returns(None))
--- tests/pdp/pdp_exam8Test.py
+++ tests/pdp/pdp_exam8Test.py
@@ -1,5 +1,6 @@
 from checkpy import *
 
+download("film.csv", "https://raw.githubusercontent.com/uvaai/pdp/2023/exam/data/film.csv")
 ######## Q1 ########
 
 fun1_def = (declarative
@@ -52,3 +53,16 @@ population_2 = {
 expected = {'Houston': -100000, 'Chicago': 100000, 'Los Angeles': 100000, 'New York': 200000}
 
 test3_1 = test()(fun3_def.call(population_1, population_2).returns(expected))
+
+
+######## Q4 ########
+
+fun4_def = (declarative
+    .function("average_popularity_year")
+    .params("filename", "year")
+)
+fun4_def_float = fun4_def.returnType(float)
+
+test4_1 = test()(fun4_def_float.call("film.csv", 1984).returns(approx(42.47, abs=0.1)))
+test4_2 = test()(fun4_def_float.call("film.csv", 1990).returns(approx(39.42, abs=0.1)))
+test4_3 = test()(fun4_def.call("film.csv", 1994).returns(None))
