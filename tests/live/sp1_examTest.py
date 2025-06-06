c7383d59e6cd2fb5cbfa7dc1bb347f180b719f07
simonpauw <simonpauw@gmail.com>
2025-06-06 15:46:38 +0200

    sp1 sp2 final tests

---
+++ tests/live/sp1_examTest.py
@@ -0,0 +1,94 @@
+from checkpy import *
+import typing
+import pandas as pd
+
+only("sp1_exam18.py")
+download("barca.txt", "https://raw.githubusercontent.com/spcourse/exam-tests/main/data/barca.txt")
+
+
+class ApproxString:
+    def __init__(self, base_string):
+        self._base_string = base_string.strip().replace("\n", " ")
+
+    def __eq__(self, other_string):
+        other_string = other_string.strip().replace("\n", " ")
+        return self._base_string == other_string
+
+    def __repr__(self):
+        return repr(self._base_string) + "[approximately]"
+
+
+class ApproxDataFrame:
+    def __init__(self, base_df):
+        self._base_df = base_df
+
+    def __eq__(self, other_df):
+        return self._base_df.to_dict() == other_df.to_dict()
+
+    def __repr__(self):
+        return repr(self._base_df)
+
+######## Q1 ########
+
+fun1_def = (declarative
+    .function("approximate_pi")
+    .params("n")
+    .returnType(float)
+)
+
+test1_1 = test()(fun1_def
+    .call(10)
+    .returns(approx(3.04184, abs=1e-4))
+)
+
+test1_2 = test()(fun1_def
+    .call(1000)
+    .returns(approx(3.14059, abs=1e-4))
+)
+
+####### Q2 ########
+
+fun2_def = (declarative
+    .function("until_lvl")
+    .params("monsters_to_defeat", "xp_to_lvl")
+    .returnType(list)
+)
+
+monsters_to_defeat = ["goblin", "rabbit", "goblin", "goblin", "friendly NPC", "drake", "goblin", "friendly NPC", "friendly NPC", "goblin"]
+
+test2_1 = test()(fun2_def
+    .call(monsters_to_defeat, 300)
+    .returns(['goblin', 'rabbit', 'goblin', 'goblin', 'friendly NPC', 'drake', 'goblin'])
+)
+
+####### Q3 ########
+
+fun3_def = (declarative
+    .function("grocery_prices")
+    .params("base_prices", "discount_percentages")
+    .returnType(list)
+)
+
+base_prices = [7.50, 3.99, 2.99, 3.99, 4.50, 9.99, 7.69, 1.25]
+discount_percentages = [0, 20, 25, 10, 0, 60, 15, 20]
+
+discounted_prices = [7.5, 3.19, 2.24, 3.59, 4.5, 4.0, 6.54, 1.0]
+
+test3_1 = test()(fun3_def
+    .call(base_prices, discount_percentages)
+    .returns(discounted_prices)
+)
+
+####### Q4 ########
+
+fun4_def = (declarative
+    .function("number_of_boring_games")
+    .params("filename")
+    .returnType(int)
+)
+
+
+test4_1 = test()(fun4_def
+    .call('barca.txt')
+    .returns(44)
+)
---
+++ tests/live/sp2_examTest.py
@@ -0,0 +1,105 @@
+from checkpy import *
+import typing
+import pandas as pd
+
+only("sp2_exam18.py")
+download("films.csv", "https://raw.githubusercontent.com/spcourse/exam-tests/main/data/films.csv")
+
+
+class ApproxString:
+    def __init__(self, base_string):
+        self._base_string = base_string.strip().replace("\n", " ")
+
+    def __eq__(self, other_string):
+        other_string = other_string.strip().replace("\n", " ")
+        return self._base_string == other_string
+
+    def __repr__(self):
+        return repr(self._base_string) + "[approximately]"
+
+
+class ApproxDataFrame:
+    def __init__(self, base_lst):
+        self._base_lst = base_lst
+
+    def __eq__(self, other_df):
+        return self._base_lst == list(other_df["Title"])
+
+    def __repr__(self):
+        return repr(self._base_lst)
+
+######## Q1 ########
+
+fun1_def = (declarative
+    .function("passed_students")
+    .params("student_grades")
+    .returnType(list)
+)
+
+
+grades_dict = {
+"Paul": [7.0, 5.1, 6.3],
+"Livia": [7.8, 8.9, 6.4],
+"Linos": [8.3, 4.3, 9.8],
+"Corinna": [5.5, 5.3, 5.1],
+"Yasmina": [7.0, 9.1, 8.3],
+"Leonard": [3.0, 2.1, 3.3],
+"Eula": [7.6, 7.7, 3.3],
+"Shukra": [5.0, 5.0, 8.0],
+"Emil": [7.0, 5.1, 6.3],
+}
+
+passed_lst =  ['Paul', 'Livia', 'Linos', 'Yasmina', 'Eula', 'Shukra', 'Emil']
+
+
+test1_3 = test()(fun1_def
+    .call(grades_dict)
+    .returns(passed_lst)
+)
+
+####### Q2 ########
+
+fun2_def = (declarative
+    .function("count_letters")
+    .params("word")
+    .returnType(dict)
+)
+
+input_word = "elementary"
+
+test2_1 = test()(fun2_def
+    .call(input_word)
+    .returns({'e': 3, 'l': 1, 'm': 1, 'n': 1, 't': 1, 'a': 1, 'r': 1, 'y': 1})
+)
+
+fun3_def = (declarative
+    .function("is_anagram")
+    .params("word1", "word2")
+    .returnType(bool)
+)
+
+test2_2 = test()(fun3_def
+    .call('listen', 'silent')
+    .returns(True)
+)
+
+test2_3 = test()(fun3_def
+    .call('angular', 'granular')
+    .returns(False)
+)
+
+####### Q3 ########
+
+fun4_def = (declarative
+    .function("second_best_film_per_year")
+    .params("filename")
+    .returnType(pd.DataFrame)
+)
+
+expected_titles = ["Kriemhild's Revenge, The Nibelungenlied", 'Joyless Street', 'Flesh & the Devil, The', 'My Best Girl', 'Mysterious Lady, The', 'Wild Orchids', 'Anna Christie', 'Mata Hari', 'Number Seventeen', 'Baby Face', 'Judge Priest', 'Adventures of Rex & Rinty, The', 'Camille', 'Intermezzo', "A Woman's Face", 'Jamaica Inn', 'Law & Order', 'Man from Montana', 'Reap the Wild Wind', 'Ape Man, The', 'Gaslight', 'Flame of Barbary Coast', 'Gilda', 'Hawk of Powder River', 'Three Godfathers', 'She Wore a Yellow Ribbon', 'Stage Fright', 'Strangers on a Train', 'Come Back, Little Sheba', 'Mogambo', 'Inauguration of the Pleasure Dome', 'Rebel Without a Cause', 'Seventh Seal, The', 'Gunfight at the OK Corral', 'Matchmaker, The', 'Carlton-Browne of the F.O.', 'Spartacus', 'Four Horsemen of the Apocalypse, The', 'Lolita', 'Cleopatra', 'Goldfinger', 'Dear Brigitte', 'Russians Are Coming, the Russians Are, The', 'Bobo, The', 'War & Peace', 'True Grit', 'Getting Straight', 'Statue, The', 'Say Goodbye Maggie Cole', 'Dillinger', 'Monty Python & the Holy Grail', 'That Lucky Touch', 'Rocky', 'Slap Shot', 'Magic', 'Alien', 'Rodeo Girl', 'For Your Eyes Only', 'King of Comedy', 'Man with Two Brains, The', 'Romancing the Stone', 'Out of the Darkness', 'Best of Times, The', 'Matewan', 'Phantom of the Ritz', "New Year's Day", 'Guilty by Suspicion', 'Raw Nerve', 'Party Girl', 'Honeymoon in Vegas', 'Island of Dr. Moreau, The', 'Alien: resurrection']
+
+
+test3_1 = test()(fun4_def
+    .call('films.csv')
+    .returns(ApproxDataFrame(expected_titles))
+)
